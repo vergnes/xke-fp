91c9c8fcb7a165dead5212ce1f46cfcdbcb1841b
denis <denis.vergnes@orange.com>
2013-01-28 16:51:12 +0800

    Use tail recursion

--- xke-fp/src/main/java/fp/HandsOn.java
+++ xke-fp/src/main/java/fp/HandsOn.java
@@ -3,10 +3,14 @@ package fp;
 public class HandsOn {
 
 	public static long fact(int n) {
+		return fact(n, 1);
+	}
+
+	private static long fact(int n, long acc) {
 		if (n == 0) {
-			return 1;
+			return acc;
 		} else {
-			return fact(n - 1) * n;
+			return fact(n - 1, n * acc);
 		}
 	}
 
